d2a8d150b4bc11bc94c213acb16ce68833f10241
mihtri126 <minhtri120604@gmail.com>
2024-07-19 16:46:50 +0700

    update

---
+++ backend/src/main/java/bizworks/backend/dtos/ResetPasswordDTO.java
@@ -0,0 +1,12 @@
+package aptech.project.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResetPasswordDTO {
+    private String newPassword;
+}
