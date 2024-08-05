694701b1369dea8f5633c9344012e19f45d6d1b8
mihtri126 <minhtri120604@gmail.com>
2024-08-06 00:08:52 +0700

    Create EmployeeUpdateDTO.java

---
+++ backend/src/main/java/bizworks/backend/dtos/EmployeeUpdateDTO.java
@@ -0,0 +1,20 @@
+package bizworks.backend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EmployeeUpdateDTO {
+    private LocalDate dob;
+    private String address;
+    private String gender;
+    private String phone;
+    private String avatar;
+    private MultipartFile fileImage;
+}
